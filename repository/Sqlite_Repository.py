95c03829cb02615e5953fb1c063fd41bc3ee246c
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-09-04 17:10:49 -0300

    Unit tests and parameters repository clean up

--- repository/Sqlite_Repository.py
+++ repository/Sqlite_Repository.py
@@ -12,60 +12,66 @@ class SQLite_Repository:
     
     @property
     def cursor(self) -> Cursor:
-        return self._db.cursor()
+        return self.connection.cursor()
     
-    def create_table(self, connection: Connection, cursor: Cursor, table_name: str, table_schema: dict):
+    def create_table(self, table_name: str, table_schema: dict):
         schema_columns = ','.join([f"{col_name} {col_type}" for col_name, col_type in table_schema.items()])
         query = f"CREATE TABLE IF NOT EXISTS {table_name} ( {schema_columns} )"
+        cursor = self.cursor
         cursor.execute(query)
-        connection.commit()
+        self.connection.commit()
         print(f"A tabela {table_name} foi criada com sucesso!")
         cursor.close()
 
-    def findAll(self, connection: Connection, cursor: Cursor, table_name: str, fields="*"):
+    def findAll(self, table_name: str, fields="*"):
         query = f"SELECT {fields} FROM {table_name}"
+        cursor = self.cursor
         cursor.execute(query)
         results = cursor.fetchall()
         cursor.close()
         return results
 
 
-    def findOne(self, connection: Connection, cursor: Cursor, table_name: str, identifier: dict, fields="*"):
-        clause_conditions, clause_values = get_where_clause_conditions(identifier)
+    def findOne(self, table_name: str, data_identifier: dict, fields="*"):
+        clause_conditions, clause_values = get_where_clause_conditions(data_identifier)
         query = f"SELECT {fields} FROM {table_name} WHERE {clause_conditions}"
+        cursor = self.cursor
         cursor.execute(query, clause_values)
         result = cursor.fetchone()
         cursor.close()
         return result
 
-    def insert(self, connection: Connection, cursor: Cursor, table_name: str, data: dict):
+    def insert(self, table_name: str, data: dict):
         columns = ",".join(data.keys())
         placeholders = ",".join(["?"] * len(data.keys()))
         values = tuple(data.values())
         query = f"INSERT INTO {table_name} ({columns}) VALUES ({placeholders})"
+        cursor = self.cursor
         cursor.execute(query, values)
-        connection.commit()
+        self.connection.commit()
         last_id = cursor.lastrowid
         cursor.close()
         return last_id
 
 
-    def update(self, connection: Connection, cursor: Cursor, table_name: str, updated_data: dict):
+    def update(self, table_name: str, data_identifier: dict, updated_data: dict):
         columns_to_update = ",".join([f"{column}=?" for column in updated_data.keys()])
-        clause_conditions, clause_values = get_where_clause_conditions(updated_data)
+        clause_conditions, clause_values = get_where_clause_conditions(data_identifier)
         values_to_update = tuple(updated_data.values()) + clause_values
         query = f"UPDATE {table_name} SET {columns_to_update} WHERE {clause_conditions}"
+        cursor = self.cursor
         cursor.execute(query, values_to_update)
-        connection.commit()
+        self.connection.commit()
         rows_affected = cursor.rowcount
         cursor.close()
         return rows_affected
 
-    def delete(self, connection: Connection, cursor: Cursor, table_name: str, identifier: dict):
-        clause_conditions, clause_values = get_where_clause_conditions(identifier)
+    def delete(self, table_name: str, data_identifier: dict):
+        clause_conditions, clause_values = get_where_clause_conditions(data_identifier)
         query = f"DELETE FROM {table_name} WHERE {clause_conditions}"
+        cursor = self.cursor
         cursor.execute(query, clause_values)
-        connection.commit()
+        self.connection.commit()
         rows_affected = cursor.rowcount
         cursor.close()
         return rows_affected
--- repository/Test_Sqlite_Repository.py
+++ repository/Test_Sqlite_Repository.py
@@ -8,12 +8,30 @@ class Test_Sqlite_Repository(TestCase):
     @classmethod
     def setUpClass(cls):
         cls.connection_mock = Mock()
-        cls.cursor_mock = cls.connection_mock.cursor.return_value
-       
+        cls.repo_mock = SQLite_Repository(cls.connection_mock)
     
     @classmethod
     def tearDownClass(cls):
-        cls.connection_mock.close_connection()
+        if hasattr(cls.repo_mock, "close_connection"):
+            cls.repo_mock.close_connection()
+            print("Conexão mockada encerrada com sucesso!")
+
+
+    def test_connection(self):
+        connection_mock = self.connection_mock
+        repo_mock = self.repo_mock
+    
+        self.assertEqual(connection_mock, repo_mock.connection)
+
+
+    def test_cursor(self):
+        repo_mock = self.repo_mock
+        
+        cursor_mock = Mock()
+        self.connection_mock.cursor.return_value = cursor_mock
+
+        self.assertEqual(repo_mock.cursor, cursor_mock)
+
     
     def test_create_table(self):
         table_name_test = "test"
@@ -21,37 +39,110 @@ class Test_Sqlite_Repository(TestCase):
             "id": "INTEGER PRIMARY KEY",
             "firstname": "TEXT"
         }
-        connection_mock = self.connection_mock
-        cursor_mock = self.cursor_mock
-        
-        repo_mock = SQLite_Repository(connection_mock)
-
+        cursor_mock = Mock()
+        self.connection_mock.cursor.return_value = cursor_mock
+    
         with patch("builtins.print") as print_message:
-            repo_mock.create_table(connection_mock, cursor_mock, table_name_test, table_schema_test)
+            self.repo_mock.create_table(table_name_test, table_schema_test)
 
-            cursor_mock.execute.assert_called_once_with("CREATE TABLE IF NOT EXISTS test ( id INTEGER PRIMARY KEY,firstname TEXT )")
-            connection_mock.commit.assert_called_once()
+            self.repo_mock.cursor.execute.assert_called_once_with("CREATE TABLE IF NOT EXISTS test ( id INTEGER PRIMARY KEY,firstname TEXT )")
+            self.repo_mock.connection.commit.assert_called_once()
             print_message.assert_called_once_with("A tabela test foi criada com sucesso!")      
-            cursor_mock.close.assert_called_once()
+            self.repo_mock.cursor.close.assert_called_once()
 
 
     def test_findAll(self):
-        pass
+        cursor_mock  = Mock()
+        self.connection_mock.cursor.return_value = cursor_mock
+        self.connection_mock.cursor.return_value.fetchall.return_value = [("test1", ),("test2", )]
+
+        table_name = "test"
+
+        results = self.repo_mock.findAll(table_name)
+
+        self.repo_mock.cursor.execute.assert_called_once_with("SELECT * FROM test")
+        self.assertEqual(results, [("test1", ),("test2", )])
+        self.repo_mock.cursor.close.assert_called_once()
+
 
     def test_findOne(self):
-        pass
+        cursor_mock = Mock()
+        self.connection_mock.cursor.return_value = cursor_mock
+        self.connection_mock.cursor.return_value.fetchone.return_value = [("test1",)]
+
+        table_name = "test"
+        identifier = {
+            "id": "test",
+            }
+
+        result = self.repo_mock.findOne(table_name, identifier)
+
+        self.repo_mock.cursor.execute.assert_called_once_with("SELECT * FROM test WHERE id=?", ("test",))
+        self.assertEqual(result, [("test1",)])
+        self.repo_mock.cursor.fetchone.assert_called_once()
+        self.repo_mock.cursor.close.assert_called_once()
+
 
     def test_insert(self):
-        pass
+        cursor_mock = Mock()
+        self.connection_mock.cursor.return_value = cursor_mock
+        self.connection_mock.cursor.return_value.lastrowid = 1
+
+        table_name = "test"
+        data_to_insert = {
+            "id": "test",
+            "data": "test_data"
+        }
+
+        result = self.repo_mock.insert(table_name, data_to_insert)
+
+        self.repo_mock.cursor.execute.assert_called_once_with("INSERT INTO test (id,data) VALUES (?,?)", ("test", "test_data"))
+        self.repo_mock.connection.commit.assert_called()
+        self.repo_mock.cursor.close.assert_called_once()
+        self.assertEqual(result, 1)
+
 
     def test_update(self):
-        pass
+        cursor_mock = Mock()
+        self.connection_mock.cursor.return_value = cursor_mock
+        self.connection_mock.cursor.return_value.rowcount = 1
+
+        table_name = "test"
+        data_identifier = {
+            "id": "test"
+        }
+        updated_data = {
+            "id": "test_id",
+            "data": "new_data"
+        }
+
+        result = self.repo_mock.update(table_name, data_identifier, updated_data)
+        self.repo_mock.cursor.execute.assert_called_once_with("UPDATE test SET id=?,data=? WHERE id=?", ("test_id", "new_data", "test"))
+        self.repo_mock.cursor.close.assert_called_once()
+        self.assertEqual(result, 1)
+
 
     def test_delete(self):
-        pass
+        cursor_mock = Mock()
+        self.connection_mock.cursor.return_value = cursor_mock
+        self.connection_mock.cursor.return_value.rowcount = 1
+
+        table_name = "test"
+        data_identifier = {
+            "id": "test_id"
+        }
+
+        result = self.repo_mock.delete(table_name, data_identifier)
+        self.repo_mock.cursor.execute.assert_called_once_with("DELETE FROM test WHERE id=?", ("test_id",))
+        self.repo_mock.cursor.close.assert_called_once()
+        self.assertEqual(result, 1)
+
 
     def test_close_connection(self):
-        pass
+        with patch("builtins.print") as print_message:
+            self.repo_mock.close_connection()
+            print_message.assert_called_once_with("Conexão com o SQLite encerrada com sucesso!")
+            self.connection_mock.close.assert_called_once()
 
 if __name__ == "__main__":
     unittest.main()
