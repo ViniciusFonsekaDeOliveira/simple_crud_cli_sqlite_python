11d4e7da4b19fe8119d3d3f519f4153e39954e46
Vinícius Fonseca de Oliveira <viniciusfonsekadeoliveira@gmail.com>
2023-09-04 12:50:56 -0300

    Feat: Unit test create table method

--- repository/Test_Sqlite_Repository.py
+++ repository/Test_Sqlite_Repository.py
@@ -1,17 +1,39 @@
+import unittest
 from unittest import TestCase
+from unittest.mock import Mock, patch
+from Sqlite_Repository import SQLite_Repository
 
 class Test_Sqlite_Repository(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.connection_mock = Mock()
+        cls.cursor_mock = cls.connection_mock.cursor.return_value
+       
     
     @classmethod
     def tearDownClass(cls):
-        pass
+        cls.connection_mock.close_connection()
     
     def test_create_table(self):
-        pass
+        table_name_test = "test"
+        table_schema_test = {
+            "id": "INTEGER PRIMARY KEY",
+            "firstname": "TEXT"
+        }
+        connection_mock = self.connection_mock
+        cursor_mock = self.cursor_mock
+        
+        repo_mock = SQLite_Repository(connection_mock)
+
+        with patch("builtins.print") as print_message:
+            repo_mock.create_table(connection_mock, cursor_mock, table_name_test, table_schema_test)
+
+            cursor_mock.execute.assert_called_once_with("CREATE TABLE IF NOT EXISTS test ( id INTEGER PRIMARY KEY,firstname TEXT )")
+            connection_mock.commit.assert_called_once()
+            print_message.assert_called_once_with("A tabela test foi criada com sucesso!")      
+            cursor_mock.close.assert_called_once()
+
 
     def test_findAll(self):
         pass
@@ -31,3 +53,5 @@ class Test_Sqlite_Repository(TestCase):
     def test_close_connection(self):
         pass
 
+if __name__ == "__main__":
+    unittest.main()
